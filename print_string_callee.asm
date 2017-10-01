b22a3bbb253f919ccb32abd0e0686595370ceafb
Megidd <megiddgit@gmail.com>
2017-10-01 13:25:25 +0330

    Finish developing print_string caller and callee

--- print_string_callee.asm
+++ print_string_callee.asm
@@ -7,8 +7,14 @@ print_string:
 			;therefore 0x7c00 needs to be added to the data address
 			;to get the correct absolute data address
 	
+print_next_byte:
 	mov al,[bx]	;bx is used as function parameter
+	cmp al,0x00
+	je null_terminated
 	int 0x10
+	inc bx
+	jmp print_next_byte
 	
+null_terminated:
 	popa	;pop all registers to be polite
 	ret	;pop return address and jump to it
