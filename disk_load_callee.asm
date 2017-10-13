a1a7d920210c11f724fc44f1d9814ff046edece1
Megidd <megiddgit@gmail.com>
2017-10-13 14:45:40 +0330

    Finish developing disk_load code

--- disk_load_callee.asm
+++ disk_load_callee.asm
@@ -1,19 +1,50 @@
 disk_load:
  pusha
 
- mov ah, 0x02		;BIOS read sector function
- mov dl, 0x00		;first floppy drive
- mov ch, 0x03		;select cylinder 3
- mov dh, 0x01		;Select the track on 2nd side of floppy
+ push dx		; Store  DX on  stack  so  later  we can  recall
+			; how  many  sectors  were  request  to be read ,
+			; even if it is  altered  in the  meantime
+ 
+ mov al, dh		; DX is input, DH stores how many sectors to read
+
+ mov ah, 0x02		; BIOS read sector function
+
+; mov dl, 0x00		; first floppy drive
+			; DL stores boot_drive, set by the caller
+
+; DL is already set by the caller as BOOT_DRIVE
+
+; mov ch, 0x03		; select cylinder 3
+
+ mov ch, 0x00		; cylinder 0
+
+; mov dh, 0x01		; Select the track on 2nd side of floppy
 			; disk , since this count has a base of 0
- mov cl, 0x04		;Select  the 4th  sector  on the  track  - not
+
+ mov dh, 0x00		; Select head 0
+
+; mov cl, 0x04		; Select  the 4th  sector  on the  track  - not
 			; the 5th , since  this  has a base of 1.
- mov al, 0x05
- mov bx, 0xa000
- mov es,bx
- mov bx,0x1234
+
+ mov cl, 0x02		; Start  reading  from  second  sector (i.e.
+			; after  the  boot  sector)
+
+; BX is already set by the caller as the destination address
 
  int 0x13
+ jc disk_error
 
+ pop dx
+ cmp dh, al		; if AL (sectors read) != DH (sectors expected)
+ jne disk_error
+ 
  popa
  ret
+
+ disk_error:
+ 	mov bx, DISK_ERROR_MSG
+ 	call print_string
+	jmp $		; jump forever to the same line
+
+ DISK_ERROR_MSG:
+ 	db 'Disk read error',0x00
--- disk_load_caller.asm
+++ disk_load_caller.asm
@@ -1,22 +1,35 @@
 mov bx, MSG			;bx stores print_string input
 call print_string
 
+mov [BOOT_DRIVE], dl		;BIOS  stores  our  boot  drive  in DL, so it’s
+				; best to  remember  this  for  later.
+mov bp, 0x8000
+mov sp, bp			;Here we set our stack safely out of the way , at 0x8000
+mov bx, 0x9000			; Load to destination address 0x0000(ES):0x9000(BX)
+mov dh, 0x05			; Read 5 sectors
+mov dl, [BOOT_DRIVE]
 call disk_load
-jc disk_error
 
-mov dx, 0x1ffe			;dx stores print_hex input
+mov dx, [0x9000]		;dx stores print_hex input
+				;dx stores the first loaded word at address 0x9000
 call print_hex			;print_hex modifies HEX_OUT content according to dx
 mov bx,HEX_OUT
-call print_string
+call print_string		;print 2nd sector loaded at address 0x9000
 
-disk_error:
- mov bx, DISK_ERROR_MSG
- call print_string
+mov dx, [0x9000+511]
+call print_hex
+mov bx,HEX_OUT
+call print_string		;print the margin between 2nd sector and 3rd sector loaded
+
+mov dx, [0x9000+512]
+call print_hex
+mov bx,HEX_OUT
+call print_string		;print 3rd sector loaded at address 0x9000+512
 
 jmp $
 
-DISK_ERROR_MSG:
- db 'Disk read error',0x00
+BOOT_DRIVE:
+ db 0x00
 
 %include "disk_load_callee.asm"
 
@@ -34,3 +47,7 @@ MSG:
 times 510-($-$$) db 0x00	;padding
 
 dw 0xaa55			;Boot sector signature
+				;at the end of 1st sector, i.e. boot sector
+
+times 512 db 0xee		;2nd sector filled with 0xee
+times 512 db 0xff		;3rd sector filled with 0xff
