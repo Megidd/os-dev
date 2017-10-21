d783371836436fb32d3e50bd107eeadbe805c712
Megidd <megiddgit@gmail.com>
2017-10-21 16:58:17 +0330

    Increment segment register ES and loop over segments too

--- search_string_on_memory.asm
+++ search_string_on_memory.asm
@@ -1,32 +1,57 @@
 mov bx, MSG
 call print_string
 
-xor di, di			;clear DI which is loop counter over memory addresses
-xor si, si			;clear SI which stores the address of 'BIOS' string if any found
-start_search_string:
- cmp byte [di], 'B'		;only BX/BP/DI/SI registers can be used for addressing in 16-bit real mode
- jne continue_search_string
- mov si, di			;store 'B' address in SI for later use
- inc di
- cmp byte [di], 'I'
- jne continue_search_string
- inc di
- cmp byte [di], 'O'
- jne continue_search_string
- inc di
- cmp byte [di], 'S'
- jne continue_search_string
- mov dx, si			;DX is print_hex input
- call print_hex			;print SI which is start address of 'BIOS' string
- mov bx, HEX_OUT		;BX is print_string input
- call print_string
+mov cx, 0x0000
+mov es, cx			;clear ES, i.e. segment register indirectly 
+				; => processor limitation in 16-bit real mode
+
+next_seg_reg:
+ xor di, di			;clear DI which is loop counter over memory addresses
+ xor si, si			;clear SI which stores the address of 'BIOS' string if any found
+ start_search_string:
+  cmp byte [es:di], 'B'		;only BX/BP/DI/SI registers can be used for addressing in 16-bit real mode
+				; => processor limitation at 16-bit real mode
+  jne continue_search_string
+  mov si, di			;store 'B' address in SI for later use
+  inc di
+  cmp byte [es:di], 'I'
+  jne continue_search_string
+  inc di
+  cmp byte [es:di], 'O'
+  jne continue_search_string
+  inc di
+  cmp byte [es:di], 'S'
+  jne continue_search_string
+  mov dx, si			;DX is print_hex input
+  call print_hex
+  mov bx, HEX_OUT
+  call print_string		;print SI which is start address of 'BIOS' string
+  mov bx, MSG_ADDRESS
+  call print_string
+  mov dx, es			;DX is print_hex input;
+  call print_hex
+  mov bx, HEX_OUT
+  call print_string		;print ES which is extra data segment register
+  mov bx, MSG_SEG_REG
+  call print_string
 
  continue_search_string:
   inc di
   cmp di, 0xffff
   jne start_search_string
+  mov cx, es			;increment ES indirectly => processor limitation at 16-bit real mode
+  inc cx
+  mov es, cx
+  cmp cx, 0xffff
+  jne next_seg_reg
 
 jmp $
+
+MSG_SEG_REG:
+ db ' -> Segment register',0x00
+
+MSG_ADDRESS:
+ db ' -> Address',0x00
 
 FINDME:
  db 'BIOS',0x00
