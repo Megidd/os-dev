7f43ec2875eebd57c89413b578216a7322c2efd7
Megidd <megiddgit@gmail.com>
2017-11-22 16:09:08 +0330

    Develop a more sophisticated kernel code!

--- bootstrap_kernel/kernel/kernel.c
+++ bootstrap_kernel/kernel/kernel.c
@@ -6,7 +6,25 @@ void main(){
 	 * 0d3360 = 0x0d20
 	 * Video Graphics Array (VGA) colour text mode with dimmensions 80x25 characters
 	 */
-	*video_memory='X';
+	*video_memory='K';
 	*(video_memory+1)=0x0f;//white on black
+	*(video_memory+2)='e';
+	*(video_memory+4)='r';
+	*(video_memory+6)='n';
+	*(video_memory+8)='e';
+	*(video_memory+10)='l';
+	*(video_memory+12)=' ';
+	*(video_memory+14)='i';
+	*(video_memory+15)=0x05;//Magenta
+	*(video_memory+16)='s';
+	*(video_memory+18)=' ';
+	*(video_memory+20)='r';
+	*(video_memory+21)=0x0d;//Light Magenta
+	*(video_memory+22)='u';
+	*(video_memory+24)='n';
+	*(video_memory+26)='n';
+	*(video_memory+28)='i';
+	*(video_memory+30)='n';
+	*(video_memory+32)='g';
 	return;
 }
